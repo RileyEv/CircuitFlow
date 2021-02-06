344824412938089d00b6288eca1f736598074e13
Riley Evans <riley@rileyevans.co.uk>
2021-02-06 19:43:45 +0000

    OMG it works... Thats a 3 day type error fixed :weary:

--- Pipeline/Backend/GraphMachine.hs
+++ Pipeline/Backend/GraphMachine.hs
@@ -4,65 +4,65 @@ module Pipeline.Backend.GraphMachine (
   
 ) where
 
-import Data.Typeable (Typeable(..), cast)
-import Data.List (elemIndex)
-import Pipeline.Core.Task (Task, DataSource, VariableStore(..), functionTask)
+-- import Data.Typeable (Typeable(..), cast)
+-- import Data.List (elemIndex)
+-- import Pipeline.Core.Task (Task, DataSource, VariableStore(..), functionTask)
 
-type Edge = (Int, Int) -- (from, to) use index from array of nodes
-data Graph a = Graph [a] [Edge]
-
-
-data Node = forall i j b. (Typeable i, Typeable j, Typeable b) => TaskNode (Task i b j)
-          | forall m a i. (Typeable i, Typeable m, Typeable a, Monad m, DataSource m a i) => DataNode (m (a i))  deriving Typeable
-
--- Graph with just one node inside it. Stores a Task. 
--- exampleGraph :: Gr TaskNode ()
--- exampleGraph = ([], 1, Data (return (Var 1) :: IO (VariableStore Int)), [] :: [((), Node)]) & empty
+-- type Edge = (Int, Int) -- (from, to) use index from array of nodes
+-- data Graph a = Graph [a] [Edge]
 
 
-exampleGraph' :: Graph Node
-exampleGraph' = Graph [DataNode (return (Var 1) :: IO (VariableStore Int))] []
+-- data Node = forall i j b. (Typeable i, Typeable j, Typeable b) => TaskNode (Task i b j)
+--           | forall m a i. (Typeable i, Typeable m, Typeable a, Monad m, DataSource m a i) => DataNode (m (a i))  deriving Typeable
+
+-- -- Graph with just one node inside it. Stores a Task. 
+-- -- exampleGraph :: Gr TaskNode ()
+-- -- exampleGraph = ([], 1, Data (return (Var 1) :: IO (VariableStore Int)), [] :: [((), Node)]) & empty
 
 
-atobGraph :: Graph Node
-atobGraph = Graph [
-  DataNode (return (Var 1) :: IO (VariableStore Int)), -- index 0
-  TaskNode (functionTask ((+1) :: Int -> Int)) -- index 1
-  ] [(0, 1)] -- Connect node 0 to node 1.   0 --> 1
+-- exampleGraph' :: Graph Node
+-- exampleGraph' = Graph [DataNode (return (Var 1) :: IO (VariableStore Int))] []
 
 
-makeStep :: [Int] -> Graph Node -> (Int, Graph Node)
-makeStep [] _ = error "unable to make a step"
-makeStep (uv:uvs) (Graph ns es)
-  | isUnblocked = (uv, Graph ns' es)
-  | otherwise = makeStep uvs (Graph ns es)
-    where
-      connectedEdges :: [Edge]
-      connectedEdges = filter (\(_, to) -> to == uv) es
-      parentNodes :: [Int]
-      parentNodes = map fst connectedEdges
-      isUnblocked :: Bool
-      isUnblocked = foldr (
-        \n unblocked -> (
-          case ns!!n of
-            TaskNode _ -> False
-            DataNode _ -> True) && unblocked
-        ) True parentNodes
-      ns' :: [Node]
-      ns' = foldr f [] (zip [0..] ns) -- replace uv with DataNode containing the DataSource.
-      f :: (Int, Node) -> [Node] -> [Node]
-      f (i, n) ns
-        | i == uv   = computeNode i n : ns
-        | otherwise =  n:ns
-      computeNode :: Int -> Node -> Node
-      computeNode i (TaskNode t) =
-        let parents = map (ns!!) parentNodes :: [Node]
-            firstParent = head parents :: Node
-        in case firstParent of
-          TaskNode _ -> error "shouldn't have a task node here"
-          DataNode d -> case cast d of
-            Nothing -> error ""
-            Just d1 -> case cast t of
-              Nothing -> error ""
-              Just t1 -> DataNode (t1 d1 Empty)
+-- atobGraph :: Graph Node
+-- atobGraph = Graph [
+--   DataNode (return (Var 1) :: IO (VariableStore Int)), -- index 0
+--   TaskNode (functionTask ((+1) :: Int -> Int)) -- index 1
+--   ] [(0, 1)] -- Connect node 0 to node 1.   0 --> 1
+
+
+-- makeStep :: [Int] -> Graph Node -> (Int, Graph Node)
+-- makeStep [] _ = error "unable to make a step"
+-- makeStep (uv:uvs) (Graph ns es)
+--   | isUnblocked = (uv, Graph ns' es)
+--   | otherwise = makeStep uvs (Graph ns es)
+--     where
+--       connectedEdges :: [Edge]
+--       connectedEdges = filter (\(_, to) -> to == uv) es
+--       parentNodes :: [Int]
+--       parentNodes = map fst connectedEdges
+--       isUnblocked :: Bool
+--       isUnblocked = foldr (
+--         \n unblocked -> (
+--           case ns!!n of
+--             TaskNode _ -> False
+--             DataNode _ -> True) && unblocked
+--         ) True parentNodes
+--       ns' :: [Node]
+--       ns' = foldr f [] (zip [0..] ns) -- replace uv with DataNode containing the DataSource.
+--       f :: (Int, Node) -> [Node] -> [Node]
+--       f (i, n) ns
+--         | i == uv   = computeNode i n : ns
+--         | otherwise =  n:ns
+--       computeNode :: Int -> Node -> Node
+--       computeNode i (TaskNode t) =
+--         let parents = map (ns!!) parentNodes :: [Node]
+--             firstParent = head parents :: Node
+--         in case firstParent of
+--           TaskNode _ -> error "shouldn't have a task node here"
+--           DataNode d -> case cast d of
+--             Nothing -> error ""
+--             Just d1 -> case cast t of
+--               Nothing -> error ""
+--               Just t1 -> DataNode (t1 d1 Empty)
 
--- Pipeline/Core/Task.hs
+++ Pipeline/Core/Task.hs
@@ -1,9 +1,16 @@
 {-# LANGUAGE RankNTypes, MultiParamTypeClasses, FlexibleInstances, GADTs, KindSignatures, DataKinds, FlexibleContexts #-}
 module Pipeline.Core.Task (
-  Task(..)
+  Task(..),
+  VariableStore(..),
+  DataSource(..),
+  DataSink(..),
+  Node(..),
+  processGraph,
+  testGraph,
+  functionTask,
 ) where
 
-import Data.Typeable (Typeable(..), cast, gcast)
+import Data.Typeable (Typeable(..), cast, gcast, eqT, (:~:)(..))
 
 
 -- a i only has to be a source as it is never saved to
@@ -13,64 +20,78 @@ data Task a b = (DataSource VariableStore a, DataSink VariableStore b, DataSourc
 
 
 
-class (Typeable a, Typeable f) => DataSource f a where
+class Typeable a => DataSource f a where
   fetch :: f a -> IO a
 
-class (Typeable a, Typeable f) => DataSink f a where
+class Typeable a => DataSink f a where
   -- First argument depends on the instance. It may be 'empty' or it could be a pointer to a storage location.
   save :: f a -> a -> IO (f a) 
 
+getInt :: Typeable a => a -> Int
+getInt x = case cast x of
+  Just x1 -> x1
+  Nothing -> error "error"
 
 -- Basic DataSource/Sink
 
-data VariableStore a = Var a | Empty deriving Typeable
+data VariableStore a = Var a | Empty
 
 instance Typeable a => DataSource VariableStore a where
-  fetch (Var x) = return x
+  fetch (Var x) = do
+    -- print (getInt x)
+    return x
   fetch Empty   = error "empty source"
 
 instance Typeable a => DataSink VariableStore a where
-  save _ x = return (Var x)
+  save _ x = do
+    -- print (getInt x)
+    return (Var x)
 
 
 
 
 data Node = forall a b. (Typeable a, Typeable b) =>  TaskNode (Task a b)
-          | forall a. (DataSource VariableStore a, Typeable a) => DataNode (VariableStore a)
+          | forall a. (Typeable a, DataSource VariableStore a) => DataNode (VariableStore a)
 -- data Node = forall i b j. (Typeable b, Typeable i, Typeable j) =>  TaskNode (Task i b j)
 --           | forall a i. (DataSource a i, Typeable a, Typeable i) => DataNode (a i)
 
 
 -- Lets start with a simple graph represented with a list. ie a -> b -> .. -> x
 processGraph :: (DataSource VariableStore a, Typeable a) => [Node] -> VariableStore a -> IO [Node]
-processGraph ns firstD = foldl f (return [DataNode firstD]) ns
+processGraph ns firstD = do
+  arr <- foldl f (return [DataNode firstD]) ns
+  return (tail arr)
   where
     f :: IO [Node] -> Node -> IO [Node]
-    f ds' (TaskNode (Task t)) = do
+    f ds' t = do
       ds <- ds'
-      let dn@(DataNode d) = last ds
-          nextd' = case gcast d of
-            Just d1 -> t d1 Empty
-            Nothing -> error "problem"
+      let dn = last ds
+          nextd' = test dn t
       nextd <- nextd'
-      return (ds ++ [dn, DataNode nextd])
-
-    f _ _ = error "How did it get here..."
+      return (ds ++ [nextd])
 
 
+test :: Node -> Node -> IO Node
+test (DataNode d) (TaskNode (Task t)) = do
+  r <- applyTask d (Task t)
+  return (DataNode r)
+test _ _ = error ""
 
-
--- Example typeable with existential types
-
-data Currency = IGBP | USD | AUD
-data Money (currency :: Currency) = Money Int
-data MoneyEx = forall x . Typeable x => MoneyEx (Money x)
-
-
-moneyAdd :: Money c -> Money c -> Money c
-moneyAdd (Money a) (Money b) = Money (a + b)
-
-moneyExAdd :: MoneyEx -> MoneyEx -> MoneyEx
-moneyExAdd (MoneyEx a) (MoneyEx b) = case cast a of
-  Just a1 -> MoneyEx (moneyAdd a1 b)
+applyTask :: (Typeable a, Typeable b, Typeable c) => VariableStore a -> Task b c -> IO (VariableStore c)
+applyTask d (Task t) = case gcast d of
+  Just d1 -> t d1 Empty
   Nothing -> error ""
+
+
+testGraph :: [Node]
+testGraph = [
+  TaskNode (functionTask (++ ("00" :: String))),
+  TaskNode (functionTask (read :: String -> Int)),
+  TaskNode (functionTask (+ (1 :: Int))),
+  TaskNode (functionTask (+ (1 :: Int))),
+  TaskNode (functionTask (+ (1 :: Int)))]
+
+functionTask :: (Typeable a, Typeable b) => (a -> b) -> Task a b 
+functionTask f = Task (\source sink -> do
+  input <- fetch source
+  save sink (f input))
