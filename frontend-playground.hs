ab106b68d85085323e668f42dc78f7895a3d795b
Riley Evans <riley@rileyevans.co.uk>
2021-03-14 17:17:41 +0000

    Move examples to playground

--- frontend-playground.hs
+++ frontend-playground.hs
@@ -1,61 +1,112 @@
 module Main where
 
-import Pipeline (runWorkflow)
-import Pipeline.Frontend.Pipe (Pipe(..), (>>>), (&))
-import Pipeline.Frontend.Workflow (Workflow, registerTask)
-import Pipeline.Core.Task (Task, functionTask)
-import Pipeline.Core.Node (extractLeafs)
-import Pipeline.Core.DataStore (IOStore(..), VariableStore(..), FileStore(..), CSVStore(..))
+import Pipeline.Frontend.Circuit
+
+import Pipeline.Core.Task (Task, functionTask, multiInputFunctionTask)
+import Pipeline.Core.DataStore (IOStore(..), VariableStore(..), FileStore(..), CSVStore(..), HList(..))
+
+import Prelude hiding (id, replicate, (<>))
 
 
-readIOTask :: Task IOStore String VariableStore Int
+readIOTask :: Task '[IOStore] '[String] VariableStore Int
 readIOTask = functionTask (read :: String -> Int) Empty
 
-plus1Task :: Task VariableStore Int VariableStore Int
-plus1Task = functionTask (+ (1 :: Int)) Empty
-
-showFileTask :: FilePath -> Task VariableStore Int FileStore String
+showFileTask :: FilePath -> Task '[VariableStore] '[Int] FileStore String
 showFileTask f = functionTask (show :: Int -> String) (FileStore f)
 
-replicateTask :: Task VariableStore Int VariableStore [Int]
-replicateTask = functionTask (replicate 100) Empty
+-- replicateTask :: Task '[VariableStore] '[Int] VariableStore [Int]
+-- replicateTask = functionTask (replicate 100) Empty
 
-zipWithSelf :: FilePath -> Task VariableStore [Int] CSVStore [(Int, Int)]
+zipWithSelf :: FilePath -> Task '[VariableStore] '[[Int]] CSVStore [(Int, Int)]
 zipWithSelf f = functionTask (\xs -> zip xs xs) (CSVStore f)
 
-zipWith1To100 :: FilePath -> Task VariableStore [Int] CSVStore [(Int, Int)]
+zipWith1To100 :: FilePath -> Task '[VariableStore] '[[Int]] CSVStore [(Int, Int)]
 zipWith1To100 f = functionTask (zip [1..100]) (CSVStore f)
 
-zipWith100To1 :: FilePath -> Task VariableStore [Int] CSVStore [(Int, Int)]
+zipWith100To1 :: FilePath -> Task '[VariableStore] '[[Int]] CSVStore [(Int, Int)]
 zipWith100To1 f = functionTask (zip [100, 99..1]) (CSVStore f)
 
-testWorkflow1 :: Workflow Pipe
-testWorkflow1 = do
-  readIOTask'   <- registerTask readIOTask
-  plus1Task'    <- registerTask plus1Task
-  plus1Task''   <- registerTask plus1Task
-  plus1Task'''  <- registerTask plus1Task
-  showFileTask' <- registerTask (showFileTask "testfiles/testPipeline1.out")
 
-  return $ Pipe $ 
-    readIOTask' >>> plus1Task' >>> plus1Task'' >>> plus1Task''' >>> showFileTask' 
+-- Some example tasks
+plus1Task :: Task '[VariableStore] '[Int] VariableStore Int
+plus1Task = functionTask (+1) Empty
 
+showTask :: Task '[VariableStore] '[Int] VariableStore String
+showTask = functionTask show Empty
 
-testWorkflow2 :: Workflow Pipe
-testWorkflow2 = do
-  readIOTask' <- registerTask readIOTask
-  replicateTask' <- registerTask replicateTask
-  zipWithSelf' <- registerTask (zipWithSelf "testfiles/testWorkflow2.1.out")
-  zipWith1To100' <- registerTask (zipWith1To100 "testfiles/testWorkflow2.2.out")
-  zipWith100To1' <- registerTask (zipWith100To1 "testfiles/testWorkflow2.3.out")
-
-  return $ Pipe (readIOTask' >>> replicateTask' >>> zipWithSelf')
-         & Pipe (replicateTask' >>> zipWith1To100')
-         & Pipe (replicateTask' >>> zipWith100To1')
+appendTask :: Task '[VariableStore, VariableStore] '[String, String] VariableStore String
+appendTask = multiInputFunctionTask (\(HCons x (HCons y HNil)) -> x ++ y ) Empty
 
 
-main :: IO ()
-main = do
-  _ <- runWorkflow testWorkflow1 (IOEmpty :: IOStore String)
-  _ <- runWorkflow testWorkflow2 (IOEmpty :: IOStore String)
-  return ()
+-- Example Circuits
+
+-- Example1
+--   a  
+--   |  
+--  +1
+--   |
+--  show
+--   |  
+--   b
+example1 :: Circuit '[VariableStore Int] '[VariableStore String]
+-- example = Then (Then Id (Apply plus1Task)) (Apply showTask)
+example1 = id
+          <->
+          apply plus1Task
+          <->
+          apply showTask
+
+
+-- Example 2
+--     a
+--    / \
+--   +1 show
+--    \ /
+--     /
+--    / \
+--    | |
+--    b c
+example2 :: Circuit '[VariableStore Int] '[VariableStore String, VariableStore Int]
+-- example2 = Then (Then Replicate (Beside (Apply plus1Task) (Apply showTask))) Swap
+example2 = replicate
+           <->
+           apply plus1Task <> apply showTask
+           <->
+           swap
+
+-- Example 3
+--   a      b
+--   |     / \
+--  +1    +1 show
+--  show   \ /
+--   |      /
+--   |     / \
+--   |    /  |
+--   |   /   |
+--   |  /    |
+--   \ /     | 
+--    /      |
+--   / \     |
+--   \ /     |
+--   ++      |
+--    |      |
+--    c      d
+example3 :: Circuit '[VariableStore Int, VariableStore Int] '[VariableStore String, VariableStore Int]
+example3 = example1         <> example2
+           -- VariableStore String (1), VariableStore String (2), VariableStore Int
+           <-> 
+           swap             <> id
+           -- VariableStore String (2), VariableStore String (1), VariableStore Int
+           <->
+           apply appendTask <> id
+           -- VariableStore String (2 ++ 1), VariableStore Int
+
+
+-- a few rules to note...
+-- replicate <-> dropL = id
+-- replicate <-> dropR = id
+-- replicate <-> apply f <> apply g <-> swap = replicate <-> apply g <> apply f
+-- x <> y <-> swap <-> apply f = x <> y <-> apply (flip f)
+
+-- There are probably more I will notice when using this.
+-- I think example3 could be reduced so there are no swaps. Don't thing swap can be completely eliminated.
