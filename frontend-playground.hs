66afb0572144e13b05566bd1e747b9296f8bad9d
Riley Evans <riley@rileyevans.co.uk>
2021-04-02 20:45:25 +0100

    Fix frontend playground

--- frontend-playground.hs
+++ frontend-playground.hs
@@ -4,41 +4,43 @@ import Pipeline.Frontend.Circuit
 
 import Pipeline.Core.Task (TaskF, functionTaskF, multiInputFunctionTaskF)
 import Pipeline.Core.HList
+import Pipeline.Core.Nat (Nat(..))
 import Pipeline.Core.DataStore (IOStore(..), VariableStore(..), FileStore(..), CSVStore(..))
 import Pipeline.Core.Modular ((:<:)(..))
-import Pipeline.Core.IFunctor (IFix6) 
+import Pipeline.Core.IFunctor (IFix7) 
 
 import Prelude hiding (id, replicate, (<>))
 
 
-readIOTask :: (TaskF :<: iF) => IFix6 iF '[IOStore] '[String] '[IOStore String] '[VariableStore] '[Int] '[VariableStore Int]
+readIOTask :: (TaskF :<: iF) => IFix7 iF '[IOStore] '[String] '[IOStore String] '[VariableStore] '[Int] '[VariableStore Int] ('Succ 'Zero)
 readIOTask = functionTaskF (read :: String -> Int) Empty
 
-showFileTask :: (TaskF :<: iF) => FilePath -> IFix6 iF '[VariableStore] '[Int] '[VariableStore Int] '[FileStore] '[String] '[FileStore String]
+showFileTask :: (TaskF :<: iF) => FilePath -> IFix7 iF '[VariableStore] '[Int] '[VariableStore Int] '[FileStore] '[String] '[FileStore String] ('Succ 'Zero)
 showFileTask f = functionTaskF (show :: Int -> String) (FileStore f)
 
 -- replicateTask :: Task '[VariableStore] '[Int] VariableStore [Int]
 -- replicateTask = functionTask (replicate 100) Empty
 
-zipWithSelf :: (TaskF :<: iF) => FilePath -> IFix6 iF '[VariableStore] '[[Int]] '[VariableStore [Int]] '[CSVStore] '[[(Int, Int)]] '[CSVStore [(Int, Int)]]
+zipWithSelf :: (TaskF :<: iF) => FilePath -> IFix7 iF '[VariableStore] '[[Int]] '[VariableStore [Int]] '[CSVStore] '[[(Int, Int)]] '[CSVStore [(Int, Int)]] ('Succ 'Zero)
 zipWithSelf f = functionTaskF (\xs -> zip xs xs) (CSVStore f)
 
-zipWith1To100 :: (TaskF :<: iF) => FilePath -> IFix6 iF '[VariableStore] '[[Int]] '[VariableStore [Int]] '[CSVStore] '[[(Int, Int)]] '[CSVStore [(Int, Int)]]
+zipWith1To100 :: (TaskF :<: iF) => FilePath -> IFix7 iF '[VariableStore] '[[Int]] '[VariableStore [Int]] '[CSVStore] '[[(Int, Int)]] '[CSVStore [(Int, Int)]] ('Succ 'Zero)
 zipWith1To100 f = functionTaskF (zip [1..100]) (CSVStore f)
 
-zipWith100To1 :: (TaskF :<: iF) => FilePath -> IFix6 iF '[VariableStore] '[[Int]] '[VariableStore [Int]] '[CSVStore] '[[(Int, Int)]] '[CSVStore [(Int, Int)]]
+zipWith100To1 :: (TaskF :<: iF) => FilePath -> IFix7 iF '[VariableStore] '[[Int]] '[VariableStore [Int]] '[CSVStore] '[[(Int, Int)]] '[CSVStore [(Int, Int)]] ('Succ 'Zero)
 zipWith100To1 f = functionTaskF (zip [100, 99..1]) (CSVStore f)
 
 
 -- Some example tasks
-plus1Task :: (TaskF :<: iF) => IFix6 iF '[VariableStore] '[Int] '[VariableStore Int] '[VariableStore] '[Int] '[VariableStore Int]
+plus1Task :: (TaskF :<: iF) => IFix7 iF '[VariableStore] '[Int] '[VariableStore Int] '[VariableStore] '[Int] '[VariableStore Int] ('Succ 'Zero)
 plus1Task = functionTaskF (+1) Empty
 
-showTask :: (TaskF :<: iF) => IFix6 iF '[VariableStore] '[Int] '[VariableStore Int] '[VariableStore] '[String] '[VariableStore String]
+showTask :: (TaskF :<: iF) => IFix7 iF '[VariableStore] '[Int] '[VariableStore Int] '[VariableStore] '[String] '[VariableStore String] ('Succ 'Zero)
 showTask = functionTaskF show Empty
 
 appendTask :: (TaskF :<: iF)
-  => IFix6 iF '[VariableStore, VariableStore] '[String, String] '[VariableStore String, VariableStore String] '[VariableStore] '[String] '[VariableStore String]
+  => IFix7 iF '[VariableStore, VariableStore] '[String, String] '[VariableStore String, VariableStore String]
+              '[VariableStore] '[String] '[VariableStore String] ('Succ ('Succ 'Zero))
 appendTask = multiInputFunctionTaskF (\(HCons x (HCons y HNil)) -> x ++ y ) Empty
 
 
@@ -52,7 +54,7 @@ appendTask = multiInputFunctionTaskF (\(HCons x (HCons y HNil)) -> x ++ y ) Empt
 --  show
 --   |  
 --   b
-example1 :: Circuit '[VariableStore] '[Int] '[VariableStore Int] '[VariableStore] '[String] '[VariableStore String]
+example1 :: Circuit '[VariableStore] '[Int] '[VariableStore Int] '[VariableStore] '[String] '[VariableStore String] ('Succ 'Zero)
 example1 = id
           <->
           plus1Task
@@ -69,7 +71,7 @@ example1 = id
 --    / \
 --    | |
 --    b c
-example2 :: Circuit '[VariableStore] '[Int] '[VariableStore Int] '[VariableStore, VariableStore] '[String, Int] '[VariableStore String, VariableStore Int]
+example2 :: Circuit '[VariableStore] '[Int] '[VariableStore Int] '[VariableStore, VariableStore] '[String, Int] '[VariableStore String, VariableStore Int] ('Succ 'Zero)
 example2 = replicate
            <->
            plus1Task <> showTask
@@ -95,7 +97,7 @@ example2 = replicate
 --    c      d
 example3 :: Circuit
   '[VariableStore, VariableStore] '[Int, Int] '[VariableStore Int, VariableStore Int]
-  '[VariableStore, VariableStore] '[String, Int] '[VariableStore String, VariableStore Int]
+  '[VariableStore, VariableStore] '[String, Int] '[VariableStore String, VariableStore Int] ('Succ ('Succ 'Zero))
 example3 = example1   <> example2
            -- VariableStore String (1), VariableStore String (2), VariableStore Int
            <-> 
