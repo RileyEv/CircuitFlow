e3b3f2cd51736ee82de1deeee025c7dc85cfe434
Riley Evans <riley@rileyevans.co.uk>
2021-04-26 11:38:17 +0100

    Multi file building

--- thesis/build/app/Main.hs
+++ thesis/build/app/Main.hs
@@ -3,78 +3,83 @@ module Main where
 import           Control.Monad.Trans (lift)
 import           Pipeline
 import           Prelude             hiding (read)
+import           System.FilePath     ((-<.>))
 import           System.Process
+
 
 buildDissPipeline
   :: Circuit
-       '[FileStore]
+       '[VariableStore]
+       '[[String]]
+       '[VariableStore [String]]
+       '[VariableStore]
        '[String]
-       '[FileStore String]
-       '[FileStore]
-       '[String]
-       '[FileStore String]
+       '[VariableStore String]
        N1
-buildDissPipeline = lhs2TexTask <-> buildTexTask
+buildDissPipeline = mapC lhs2TexTask Empty <-> buildTexTask
 
 lhs2TexTask
   :: Circuit
-       '[FileStore]
+       '[VariableStore]
        '[String]
-       '[FileStore String]
-       '[FileStore]
+       '[VariableStore String]
+       '[VariableStore]
        '[String]
-       '[FileStore String]
+       '[VariableStore String]
        N1
-lhs2TexTask = task f (FileStore "dissertation.tex")
+lhs2TexTask = task f (Var "dissertation.tex")
  where
   f
     :: UUID
-    -> HList' '[FileStore] '[String]
-    -> FileStore String
-    -> ExceptT SomeException IO (FileStore String)
-  f _ (HCons' (FileStore fInName) HNil') fOut@(FileStore fOutName) = do
+    -> HList' '[VariableStore] '[String]
+    -> VariableStore String
+    -> ExceptT SomeException IO (VariableStore String)
+  f _ (HCons' (Var fInName) HNil') _ = do
+    let fOutName = fInName -<.> "tex"
     lift (callCommand ("lhs2tex -o " ++ fOutName ++ " " ++ fInName ++ " > lhs2tex.log"))
-    return fOut
+    return (Var fOutName)
 
 buildTexTask
   :: Circuit
-       '[FileStore]
+       '[VariableStore]
+       '[[String]]
+       '[VariableStore [String]]
+       '[VariableStore]
        '[String]
-       '[FileStore String]
-       '[FileStore]
-       '[String]
-       '[FileStore String]
+       '[VariableStore String]
        N1
-buildTexTask = task f (FileStore "dissertation.pdf")
+buildTexTask = task f Empty
  where
   f
     :: UUID
-    -> HList' '[FileStore] '[String]
-    -> FileStore String
-    -> ExceptT SomeException IO (FileStore String)
-  f _ (HCons' (FileStore fInName) HNil') fOut@(FileStore _) = do
+    -> HList' '[VariableStore] '[[String]]
+    -> VariableStore String
+    -> ExceptT SomeException IO (VariableStore String)
+  f _ (HCons' (Var _) HNil') _ = do
     lift
       (callCommand
-        ("texfot --no-stderr latexmk -interaction=nonstopmode -pdf -no-shell-escape -bibtex -jobname=dissertation "
-        ++ fInName
-        )
+        "texfot --no-stderr latexmk -interaction=nonstopmode -pdf -no-shell-escape -bibtex -jobname=dissertation dissertation.tex"
       )
-    return fOut
+    return (Var "dissertation.pdf")
+
+
+files :: [String]
+files = ["dissertation.lhs", "introduction.lhs", "background.lhs"]
 
 main :: IO ()
 main = do
   n <-
     startNetwork buildDissPipeline :: IO
       ( BasicNetwork
-          '[FileStore]
+          '[VariableStore]
+          '[[String]]
+          '[VariableStore [String]]
+          '[VariableStore]
           '[String]
-          '[FileStore String]
-          '[FileStore]
-          '[String]
-          '[FileStore String]
+          '[VariableStore String]
       )
 
-  input_ (HCons' (FileStore "dissertation.lhs") HNil') n
+  input_ (HCons' (Var files) HNil') n
 
   o <- read n
   print o
---
+++ thesis/introduction.lhs
@@ -0,0 +1,19 @@
+\documentclass[dissertation.tex]{subfiles}
+
+%include format.fmt
+%options ghci -pgmL lhs2tex -optL--pre
+
+\begin{document}
+
+
+\chapter{Introduction}\label{chap:intro}
+\todo[inline]{Write an introduction (do near the end)}
+
+\end{document}
+
+% ----- Configure Emacs -----
+%
+% Local Variables: ***
+% mode: latex ***
+% mmm-classes: literate-haskell-latex ***
+% End: ***
